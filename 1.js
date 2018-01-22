44baae3f229f99b4eabf400211f1218c562cb725
odiachenko <32524348+odiachenko@users.noreply.github.com>
2018-01-22 20:47:13 +0200

    Fixed CeasarShift lab

--- 1.js
+++ 1.js
@@ -1,37 +1,42 @@
-var caesarShift = function(str, key) {
-	
-	// Wrap the amount
-	if (key < 0)
-		return caesarShift(str, key + 26);
+var encryptCaesarShift = function(str, key) {
 
 	// Make an output variable
 	var output = '';
 
 	// Go through each character
-	for (var i = 0; i < str.length; i ++) {
-
-		// Get the character we'll be appending
+	for (var i = 0; i < str.length; i++) {
 		var c = str[i];
 
-		// If it's a letter...
-		if (c.match(/[a-z]/i)) {
+		// Get its code
+		var code = str.charCodeAt(i);
 
-			// Get its code
-			var code = str.charCodeAt(i);
-
-			// Uppercase letters
-			if ((code >= 65) && (code <= 90))
-				c = String.fromCharCode(((code - 65 + key) % 26) + 65);
-
-			// Lowercase letters
-			else if ((code >= 97) && (code <= 122))
-				c = String.fromCharCode(((code - 97 + key) % 26) + 97);
-		}
-		// Append
+		code = code + key;
+		c = String.fromCharCode(code);
 		output += c;
 	}
 	// All done!
 	return output;
 }
-var result = caesarShift("hfgdhjjj", 2);
+var decryptCaesarShift = function(str, key) {
+
+	// Make an output variable
+	var output = '';
+
+	// Go through each character
+	for (var i = 0; i < str.length; i++) {
+		var c = str[i];
+
+		// Get its code
+		var code = str.charCodeAt(i);
+
+		code = code - key;
+		c = String.fromCharCode(code);
+		output += c;
+	}
+	// All done!
+	return output;
+}
+var result = encryptCaesarShift("hfgdhjjj", 2);
 	console.log(result)
+var result2 = decryptCaesarShift(result, 2);
+	console.log(result2)
